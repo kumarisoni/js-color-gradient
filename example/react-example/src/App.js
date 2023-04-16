cddb7cdce1cb232a410183919efc9815b69ec06a
Kumari Soni <kumasoni@in.ibm.com>
2023-04-16 23:55:29 +0530

    changed example

--- example/react-example/src/App.js
+++ example/react-example/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import {customColorMatrixOptions} from './config';
-// import { getCustomGradientColorArray } from './CustomColorGradient';
 import { data } from './SampleData';
-import getCustomGradientColorArray from 'js-color-gradient';
+import {getColors} from 'js-color-gradient';
 require('./_app.scss');
 
 const App = () => {
-    let colorMartix = getCustomGradientColorArray(customColorMatrixOptions);
+    let colorMartixAllGradient = getColors(customColorMatrixOptions);
+    let colorMatrixOneConstant = getColors(customColorMatrixOptionsOneConstant);
     let tableData = data;
     return (
         <div>
+            <h4>Gradient with three colors and all gradient</h4>
             <table>
                 <thead>
                     <tr>
@@ -27,7 +28,32 @@ const App = () => {
                             <td>
                                 {data.count}
                             </td>
-                            <td style={{ backgroundColor: colorMartix[data.percentage]}}>
+                            <td style={{ backgroundColor: colorMartixAllGradient[data.percentage]}}>
+                                {data.percentage}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <h4>Gradient with 3 colors in which 2 gradient(red and blue) and 1 constant(yellow- no gradient)</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <td><h4>Item</h4></td>
+                        <td><h4>Count</h4></td>
+                        <td><h4>Percentage</h4></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map((data, i) => (
+                        <tr key={i+1}>
+                            <td>
+                                {data.item}
+                            </td>
+                            <td>
+                                {data.count}
+                            </td>
+                            <td style={{ backgroundColor: colorMatrixOneConstant[data.percentage]}}>
                                 {data.percentage}
                             </td>
                         </tr>
--- example/react-example/src/config.js
+++ example/react-example/src/config.js
@@ -1,4 +1,4 @@
-export const customColorMatrixOptions = [
+export const customColorMatrixOptionsAllGradient = [
     {
         style: 'gradient',
         range: '0-20',
@@ -11,6 +11,26 @@ export const customColorMatrixOptions = [
         range: '21-40',
         fromColor: '#8B8000',
         toColor: '#FFFFE0'
+    },
+    {
+        style: 'gradient',
+        range: '41-100',
+        fromColor: '#0000ff',
+        toColor: '#ADD8E6'
+    }
+];
+
+export const customColorMatrixOptionsOneConstant = [
+    {
+        style: 'gradient',
+        range: '0-20',
+        fromColor: '#8B0000',
+        toColor: '#FFCCCB'
+    },
+    {
+        style: 'constant',
+        range: '21-40',
+        color: '#FFFF00'
     },
     {
         style: 'gradient',
